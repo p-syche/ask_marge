68dc780e108dd930a785e12b6315043b55df2073
Aleksandra Desmurs Linczewska <desmurs.linczewska@gmail.com>
2020-06-12 01:21:24 +0200

    displaying a response message correctly!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Wit, log } from "node-wit";
 
 
 function App() {
   const client = new Wit({accessToken: 'YQKUWRTVDAGSCNGN46L3QFVD7EQJYQF2'});
+  const [ inputValue, setInputValue ] = useState("");
+  const [ witMessage, setWitMessage ] = useState("");
+
+  const manageWitResponse = data => {
+    console.log("I want to check the object!", data, data.intents[0].name )
+    if (data && data.intents && data.intents.length > 0) {
+      if (data.intents[0].name === "greeting") {
+        setWitMessage("Well hello there!");
+      } else if (data.intents[0].name === "learn_general") {
+        setWitMessage("I can tell you want to learn about some things");
+      } else if (data.intents[0].name === "learn_about_body_parts") {
+        setWitMessage("You are curious about your body.");
+      } else {
+        setWitMessage("Unfortunately I'm not sure what you want to lear about");
+      }
+    }
+    setInputValue("")
+  };
 
   const testWitAction = () => {
-    client.message('hi!', {})
+    client.message(inputValue, {})
     .then((data) => {
+      manageWitResponse(data);
       console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
     })
     .catch(console.error);
@@ -17,18 +36,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <p>the Wit message will appear here</p>
+          <p>{witMessage}</p>
+        </div>
+        <input
+          placeholder="input stuff here"
+          value={inputValue}
+          onChange={event => setInputValue(event.target.value)}
+          type="text"
+        />
         <button onClick={() => testWitAction()}>Send a message</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
