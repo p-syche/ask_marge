5c8676b290669c5b9a659f63116a047da6f6bad3
Aleksandra Desmurs Linczewska <desmurs.linczewska@gmail.com>
2020-06-11 23:41:58 +0200

    logging Wit.ai responses to the console on button click

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Wit, log } from "node-wit";
+
 
 function App() {
+  const client = new Wit({accessToken: 'YQKUWRTVDAGSCNGN46L3QFVD7EQJYQF2'});
+
+  const testWitAction = () => {
+    client.message('hi!', {})
+    .then((data) => {
+      console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+    })
+    .catch(console.error);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => testWitAction()}>Send a message</button>
         <a
           className="App-link"
           href="https://reactjs.org"
