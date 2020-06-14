dbde15b02ab430d5a892569a89e06d4e2e692137
Aleksandra Desmurs Linczewska <desmurs.linczewska@gmail.com>
2020-06-15 01:08:25 +0200

    added randomized Mlarge picture

--- src/polaroid.js
+++ src/polaroid.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { mlargePic } from "./constants/mlarge_pic";
 
 import './assets/polaroid.css';
 
 const Polaroid = ({ currentText }) => {
+  const [ currentPic, setCurrentPic ] = useState({
+    src: "",
+    url: "",
+    name: ""
+  });
+
+  useEffect(() => {
+    const randomPic = mlargePic[Math.floor(Math.random() * mlargePic.length)];
+    setCurrentPic(randomPic);
+  }, []);
+
   const currentTextCheck = () => {
     if (currentText === "intro" ||
       currentText === "continueFirst" ||
@@ -12,16 +24,14 @@ const Polaroid = ({ currentText }) => {
     ) {
       return "hide-pic";
     }
-  }
+  };
  
   return (
     <div className={`${currentTextCheck() === "hide-pic" ? "hidden" : null} polaroid-wrapper wrapper`}>
       <div className="item">
         <div className="polaroid">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/dXAhQuT.jpg" />
-          <div className="caption">
-            By Joshua Sortino
-          </div>
+          <img src={currentPic.src} alt="Computers, servers and cables" />
+          <div className="caption">{currentPic.name}</div>
         </div>
       </div>
     </div>
