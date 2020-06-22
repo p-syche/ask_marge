7b5d3b24260e3380547595ad8d23ca64a3b727d7
Aleksandra Desmurs Linczewska <desmurs.linczewska@gmail.com>
2020-06-23 00:31:38 +0200

    added avatar image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './tailwind.generated.scss';
+import './assets/app.scss';
 
 import unicornDab from "./assets/marge_avatars/unicorn_dab.png";
+import morty from "./assets/user_avatars/no_face_fb.jpg";
 
 import ChatWrapper from "./chat-wrapper";
 import ControlArrow from "./control-arrow";
@@ -9,8 +11,14 @@ import ControlArrow from "./control-arrow";
 function App() {
   return (
     <div className="container mx-auto h-full">
-      <div className="w-full h-full flex flex-row items-end pb-24 max-w-screen-lg">
-        <div className="w-1/4">This is the left-side avatar placeholder place</div>
+      <div className="w-full h-full flex flex-row items-end pb-24 max-w-screen-lg mx-auto">
+        <div className="w-1/4 relative flex justify-center">
+          <ControlArrow direction="up" />
+          <div className="avatar-container flex items-center">
+            <img src={morty} className="avatar" alt="Marge" />
+          </div>
+          <ControlArrow direction="down" />
+        </div>
         <div className="w-1/2 flex justify-center">
           <ChatWrapper />
         </div>
